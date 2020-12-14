b9879d9e5aeb307280f7395e3d4c57cea7197c8d
Nikita <Nikita.Clark@yandex.ru>
2020-12-14 20:50:08 +0300

    code refactoring 1

--- page_loader/assets.py
+++ page_loader/assets.py
@@ -8,6 +8,7 @@ tags = {'link': 'href', 'img': 'src', 'script': 'src'}
 
 
 def change_page(base_url, page, dir_path):
+    dir_path, dir_name = os.path.split(dir_path)
     soup = BeautifulSoup(page, 'html.parser')
     resources = []
     elements = list(filter(lambda x: isLocal(x, base_url),
@@ -15,9 +16,9 @@ def change_page(base_url, page, dir_path):
     for element in elements:
         tag = tags[element.name]
         link = urljoin(base_url, element.get(tag))
-        resource_path = os.path.join(dir_path, to_file_name(link))
+        resource_path = os.path.join(dir_name, to_file_name(link))
         element[tag] = resource_path
-        resources.append((link, resource_path))
+        resources.append((link, os.path.join(dir_path, resource_path)))
     return resources, soup.prettify(formatter='html5')
 
 
