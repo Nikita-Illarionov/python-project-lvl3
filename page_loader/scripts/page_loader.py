00468aa728e9b434f3fe2ec4849fa50fcacee89a
Nikita <Nikita.Clark@yandex.ru>
2020-12-15 21:08:35 +0300

    work with exceptions

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -13,7 +13,7 @@ def main():
         file_path = download(args.url, args.output)
         print(f'Page saved in {file_path}')
     except PageLoadingError as e:
-        logging.error('status code is 500 or 400')
+        logging.error(e.text)
         sys.exit(1)
     except PermissionError:
         logging.error('Not enough access rights')
